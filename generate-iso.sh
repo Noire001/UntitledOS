dc88614f4f4fb8ba1a49c466bfe5783518534cf0
Noire001 <noiredev@protonmail.com>
2021-09-16 22:18:24 +0300

    Update build configuration

--- generate-iso.sh
+++ generate-iso.sh
@@ -1,12 +1,12 @@
-mkdir -p ../build/x86_64/isofiles/boot/grub
-cp ../build/x86_64/kernel/UntitledOS.bin ../build/x86_64/isofiles/boot/UntitledOS.bin
+mkdir -p "$1"/build/x86_64/isofiles/boot/grub
+cp "$1"/build/x86_64/kernel/UntitledOS.bin "$1"/build/x86_64/isofiles/boot/UntitledOS.bin
 echo 'set timeout=0
       set default=0
       menuentry "Untitled OS" {
           multiboot2 /boot/UntitledOS.bin
           boot
-      }' > ../build/x86_64/isofiles/boot/grub/grub.cfg
-grub-mkrescue -o ../build/x86_64/UntitledOS.iso ../build/x86_64/isofiles
+      }' > "$1"/build/x86_64/isofiles/boot/grub/grub.cfg
+grub-mkrescue -o "$1"/build/x86_64/UntitledOS.iso "$1"/build/x86_64/isofiles
+wait
 echo 'cleaning up...'
-sleep 1
-rm -rf ../build/x86_64/isofiles
+rm -rf "$1"/build/x86_64/isofiles
