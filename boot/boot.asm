ef466d006f0b9d8fbf7fae6c8b19f6650907aca9
Noire001 <noiredev@protonmail.com>
2021-09-17 00:16:16 +0300

    Check loaded by multiboot, CPUID available, long mode available

--- boot/boot.asm
+++ boot/boot.asm
@@ -3,6 +3,76 @@ global start
 section .text
 bits 32
 start:
+    mov esp, stack_top
+    call check_multiboot
+    call check_cpuid
+    call check_long_mode
+
     ; print `OK` to screen
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
+
+error:
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov byte  [0xb800a], al
+    hlt
+
+check_multiboot:
+    cmp eax, 0x36d76289
+    jne .no_multiboot
+    ret
+
+.no_multiboot:
+    mov al, "0"
+    jmp error
+
+check_cpuid:
+    pushfd
+    pop eax
+
+    mov ecx, eax
+    xor eax, 1 << 21
+
+    push eax
+    popfd
+
+    pushfd
+    pop eax
+
+    push ecx
+    popfd
+
+    cmp eax,ecx
+    je .no_cpuid
+    ret
+
+.no_cpuid:
+    mov al, "1"
+    jmp error
+
+check_long_mode:
+    mov eax, 0x80000000
+    cpuid
+    cmp eax, 0x80000001
+    jb .no_long_mode
+
+    mov eax, 0x80000001
+    cpuid
+    test edx, 1 << 29
+    jz .no_long_mode
+    ret
+
+.no_long_mode:
+    mov al, "2"
+    jmp error
+
+
+
+section .bss
+stack_bottom:
+    resb 64
+
+stack_top:
+
--- generate-iso.sh
+++ generate-iso.sh
@@ -1,11 +1,6 @@
 mkdir -p "$1"/build/x86_64/isofiles/boot/grub
 cp "$1"/build/x86_64/kernel/UntitledOS.bin "$1"/build/x86_64/isofiles/boot/UntitledOS.bin
-echo 'set timeout=0
-      set default=0
-      menuentry "Untitled OS" {
-          multiboot2 /boot/UntitledOS.bin
-          boot
-      }' > "$1"/build/x86_64/isofiles/boot/grub/grub.cfg
+cp "$1"/boot/grub.cfg "$1"/build/x86_64/isofiles/boot/grub/grub.cfg
 grub-mkrescue -o "$1"/build/x86_64/UntitledOS.iso "$1"/build/x86_64/isofiles
 wait
 echo 'cleaning up...'
